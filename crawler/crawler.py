073ee3bcfc60a3a240daa03bb9cb84fb12fcfea8
Jan Dufek <jan.dufek2002@gmail.com>
2024-01-14 15:12:04 +0100

    Added comments and fixed db config

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -5,16 +5,20 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.signalmanager import dispatcher
 
 from spider import SrealitySpider
+
 from database.database import DatabaseSreality
 
 def get_results(config):
+    """ Get result from crawler based on config """
+    
+    # connect dispatcher to save each property object into results
     results = []
-
     def spider_results(signal, sender, item, response, spider):
         results.append(item)
 
     dispatcher.connect(spider_results, signal=signals.item_scraped)
 
+    # run spider in process model
     process = CrawlerProcess(settings=dict(config['settings']))
     process.crawl(SrealitySpider, config=config['spider'])
     process.start()
@@ -23,12 +27,21 @@ def get_results(config):
 
 @hydra.main(version_base=None, config_path='../conf', config_name='config')
 def main(config):
-
     crawler_conf, db_conf = config['crawler'], config['db']
+
     results = get_results(crawler_conf)
 
-    print(len(results))
+    # create db
+    db = DatabaseSreality(db_conf['db'])
+    db.create_db()
+
+    # put scraped data into db
+    for property in results:
+        db.insert_property(property)
+    db.commit()
     
+    db.close()
+
 
 if __name__ == '__main__':
     main()
--- crawler/spider.py
+++ crawler/spider.py
@@ -1,7 +1,6 @@
 import scrapy
 
-# https://www.sreality.cz/en/search/for-sale/apartments?page=1
-# https://www.sreality.cz/api/en/v2/estates?page=1
+# https://www.sreality.cz/en/search/for-sale/apartments?noredirect=1&page=1
 
 get_exact_url = lambda url, page: f"{url}?noredirect=1&page={page}"
 
@@ -18,21 +17,26 @@ class SrealitySpider(scrapy.Spider):
 
 
     def start_requests(self):
+        """ Start request spider func """
         yield scrapy.Request(url=get_exact_url(self.url, self.page), meta=self.meta, callback=self.parse)
 
     def parse(self, response):
-        
+        """ Parse each scraped page """
+
+        # get property elements
         for property in response.css(".property"):
             yield {
                 'title' : property.css(".name::text").get(),
                 'img_url': property.css("img::attr(src)").getall()[0]
             }
 
+            # set running = False if over limit
             self.count += 1
             if self.count > self.limit:
                 self.running = False
                 break
         
+        # if under limit, scrape another page
         if self.running:
             self.page += 1
             yield scrapy.Request(url=get_exact_url(self.url, self.page), meta=self.meta, callback=self.parse)
